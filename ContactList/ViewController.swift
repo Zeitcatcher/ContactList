2a650c7ce34d1ee1319cd104f738571cbcbe02a4
Arseniy Oksenoyt <a.oksenoyt@gmail.com>
2023-08-25 15:46:17 -0700

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Arseniy Oksenoyt on 8/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
